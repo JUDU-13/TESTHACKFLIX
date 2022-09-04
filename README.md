5a0a143270467114489a23e6ca4e96de8318312c
JAIDON GILL SHAJAN <84973988+JUDU-13@users.noreply.github.com>
2022-09-04 19:01:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hackflix
+
+Just a Google Form Alternative with form validation and still didn't connected to the spreadsheet(Cheapest Local Database that available to me). I'll come back later
+
+[Basic Page](https://rajatsandeepsen.github.io/Hackflix/form/registration.html)
---
+++ form/brain.js
@@ -0,0 +1,59 @@
+function Opener() {
+  
+//google form connections
+
+
+    document.getElementById("darker").style.display = "block";
+    document.getElementById("popup").style.display = "flex" ;
+}
+
+function retry(){
+   document.getElementById("after").style.display="none";
+  document.getElementById("right").style.display="flex";
+  document.getElementById("form").style.display="flex";
+  document.getElementById("form").reset();
+}
+function closeright() {
+    document.getElementById("right").style.display="none";
+    document.getElementById("leftopenbutton").style.display="block";
+    document.getElementById("leftopenbutton2").style.display="block";
+}
+
+function Showright() {
+    document.getElementById("right").style.display="flex";
+    document.getElementById("leftopenbutton").style.display="none";
+    document.getElementById("leftopenbutton2").style.display="none";
+}
+
+function Closer() {
+    document.getElementById("leftopenbutton").style.display="none";
+    document.getElementById("leftopenbutton2").style.display="none";
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("darker").style.display = "none";
+    document.getElementById("right").style.display="flex";
+    document.getElementById("form").style.display="none";
+    document.getElementById("after").style.display="flex";
+}
+
+//for timer
+var countDownDate = new Date("Oct 20, 2022 12:00:00").getTime();
+var x = setInterval(function()
+{
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  document.getElementById("timer").innerHTML = "HACKFLIX 2022 will starts in" + "<BR>" + days + "<b> days </b> " + hours + " hours "+ minutes + " mins " + seconds + " sec";
+
+
+    if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "HACKFLIX 2022 STARTED";
+        }
+
+}, 1000);
